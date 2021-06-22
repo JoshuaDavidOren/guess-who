ede25b54ee7a987c4c84df0f8e7c7e639480bd13
Joshua Oren <joshuadavidoren@gmail.com>
2021-06-21 20:04:04 -0500

    files linked css, js, pictureshould reflect individuals

--- client.js
+++ client.js
@@ -1,1 +1,6 @@
-console.log('Here are all the available people:', people);
+$(document).ready(start);
+
+function start() {
+    console.log('JQ');
+    console.log('Here are all the available people:', people);
+}
--- data.js
+++ data.js
@@ -1,30 +1,37 @@
-var people = [
-  {
-    name: 'Dev',
-    githubUsername: 'devjanaprime'
-  },
-  {
-    name: 'Dane',
-    githubUsername: 'DoctorHowser'
-  },
-  {
-    name: 'Mary',
-    githubUsername: 'mbmosman'
-  },
-  {
-    name: 'Kris',
-    githubUsername: 'kdszafranski'
-  },
-  {
-    name: 'Edan',
-    githubUsername: 'eschwartz'
-  },
-  {
-    name: 'Casie',
-    githubUsername: 'cassandradanger'
-  },
-  {
-    name: 'Chris',
-    githubUsername: 'christopher-black'
-  },
+var people = [{
+        name: 'Dev',
+        githubUsername: 'devjanaprime',
+        img: `<img src="https://github.com/devjanaprime.png?size=250" alt="Profile image of devjanaprime">`
+    },
+    {
+        name: 'Dane',
+        githubUsername: 'DoctorHowser',
+        img: `<img src="https://github.com/DoctorHowser.png?size=250" alt="Profile image of DoctorHowser">`
+
+    },
+    {
+        name: 'Mary',
+        githubUsername: 'mbmosman',
+        img: `<img src="https://github.com/mbmosman.png?size=250" alt="Profile image of mbmosman">`
+    },
+    {
+        name: 'Kris',
+        githubUsername: 'kdszafranski',
+        img: `<img src="https://github.com/kdszafranski.png?size=250" alt="Profile image of kdszafranski">`
+    },
+    {
+        name: 'Edan',
+        githubUsername: 'eschwartz',
+        img: `<img src="https://github.com/eschwartz.png?size=250" alt="Profile image of eschwartz">`
+    },
+    {
+        name: 'Casie',
+        githubUsername: 'cassandradanger',
+        img: `<img src="https://github.com/cassandradanger.png?size=250" alt="Profile image of cassandra">`
+    },
+    {
+        name: 'Chris',
+        githubUsername: 'christopher-black',
+        img: `<img src="https://github.com/christopher-black.png?size=250" alt="Profile image of Chris">`
+    },
 ];
