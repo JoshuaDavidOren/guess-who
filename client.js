32711c17ddb5054989f40b5af52bc19a3d5cd466
Joshua Oren <joshuadavidoren@gmail.com>
2021-06-21 20:39:39 -0500

    cant figure out how to access specifice data to be append into the dom

--- client.js
+++ client.js
@@ -3,4 +3,20 @@ $(document).ready(start);
 function start() {
     console.log('JQ');
     console.log('Here are all the available people:', people);
+
 }
+
+
+
+
+
+
+
+
+
+
+//for (const person of people) {
+//     $('#body').append(`<div>`
+//         $ { person.img }
+//         `</div>`)
+//  }
