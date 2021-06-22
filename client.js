26e1dc8b48f9121734faeccb63ac9ce49cd3aa14
Joshua Oren <joshuadavidoren@gmail.com>
2021-06-21 22:36:23 -0500

    with help from higgs got the click to regester with the random name, and learned about alert(game changer)

--- client.js
+++ client.js
@@ -5,15 +5,25 @@ function start() {
     console.log('Here are all the available people:', people);
     who()
     prompt()
+    $("#body").on("click", "div", theChoice);
 }
+let thePrompt = -1;
 
 function prompt() {
     let rPerson = randomNumber(0, 6);
-    let thePrompt = people[rPerson];
+    thePrompt = people[rPerson];
     console.log(thePrompt);
     $('#prompt').empty();
     $('#prompt').append('<h2>' + thePrompt.name + '</h2>')
 
+
+}
+
+function theChoice() {
+    if (thePrompt.name === $(this).attr('id')) {
+        alert('Thats it you found them').setTimeout('3');
+        prompt();
+    } else alert('not that person, try again!')
 }
 
 function randomNumber(min, max) {
